f4638cb2b6ad6297b64eacaff6a4b5a010cf16c0
MOKSH MANRAL <moksh@MOKSHs-MacBook-Air.local>
2025-09-03 12:22:30 +0530

    chatbot react app

---
+++ src/Components/ChatInput.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import { Chatbot } from 'supersimpledev'
+import LoadingSpinnerImage from '../assets/loading-spinner.gif';
+import './ChatInput.css'
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+
+  const [inputText, setInputText] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  async function sendMessage() {
+
+    if (isLoading || inputText === '') {
+      return;
+    }
+
+    setIsLoading(true);
+
+    setInputText('');
+
+    const newChatMessages = [
+      ...chatMessages, {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      }
+    ]
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: <img className='loading-spinner' src={LoadingSpinnerImage} />,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    const response = await Chatbot.getResponseAsync(inputText);
+    setChatMessages([
+      ...newChatMessages, {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+    setIsLoading(false);
+  }
+
+  function keyPress(event) {
+    event.key === 'Enter' && sendMessage();
+    event.key === 'Escape' && setInputText('');
+  }
+
+  return (
+    <div className='chat-input-container'>
+      <input
+        placeholder="Send a Message To ChatBot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={keyPress}
+        value={inputText}
+        className='input-box'
+      />
+      <button
+        onClick={sendMessage}
+        className='send-button'
+      >Send</button>
+    </div>
+  );
+}
